98c3cd4923c6ceb200145f395305f79ef2f5c59a
umer987 <159330908+umer987@users.noreply.github.com>
2025-02-05 23:26:18 +0500

    Update env.ts

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -19,7 +19,7 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 );
 
-function assertValue<>(v: T | undefined, errorMessage: string): T {
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined || v === '') {
     throw new Error(errorMessage);
   }
